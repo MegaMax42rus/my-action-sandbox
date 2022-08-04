41e218c0f54d37ac5d66e4777445992d130e6777
MegaMax42rus <megamax42rus@gmail.com>
2022-08-04 20:10:03 +0700

    2

--- .github/actions/version-incriment-action/entrypoint.sh
+++ .github/actions/version-incriment-action/entrypoint.sh
@@ -3,6 +3,10 @@
 git config --global --add safe.directory /github/workspace
 git tag
 git describe --tags
+if [ -z $(git describe --tags | grep -Po '^(v\d.\d.)(\d)-\d-.*' | cut -d. -f3 | cut -d- -f1) ];
+	then
+		exit 0;
+fi
 echo "$(git describe --tags | grep -Po '^(v\d.\d.)(\d)-\d-.*' | cut -d. -f1,2).$(echo "$(git describe --tags | grep -Po '^(v\d.\d.)(\d)-\d-.*' | cut -d. -f3 | cut -d- -f1) + 1 " | bc)"
 git tag "$(git describe --tags | grep -Po '^(v\d.\d.)(\d)-\d-.*' | cut -d. -f1,2).$(echo "$(git describe --tags | grep -Po '^(v\d.\d.)(\d)-\d-.*' | cut -d. -f3 | cut -d- -f1) + 1 " | bc)"
 git push --tags
