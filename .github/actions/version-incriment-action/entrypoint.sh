1d7232f3c842c975fad67ac7de7d643d5c589429
MegaMax42rus <megamax42rus@gmail.com>
2022-08-04 19:48:42 +0700

    2

--- .github/actions/version-incriment-action/entrypoint.sh
+++ .github/actions/version-incriment-action/entrypoint.sh
@@ -3,3 +3,4 @@
 git config --global --add safe.directory /github/workspace
 git tag
 git describe --tags
+echo "$(git describe --tags | grep -Po '^(v\d.\d.)(\d)-\d-.*' | cut -d. -f1,2).$(echo "$(git describe --tags | grep -Po '^(v\d.\d.)(\d)-\d-.*' | cut -d. -f3 | cut -d- -f1) + 1 " | bc)"
