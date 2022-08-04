2518755c0e800b71ea8aa92e2e99a5f910cf670c
MegaMax42rus <megamax42rus@gmail.com>
2022-08-04 22:08:57 +0700

    2

--- .github/actions/version-incriment-action/entrypoint.sh
+++ .github/actions/version-incriment-action/entrypoint.sh
@@ -3,11 +3,11 @@
 git config --global --add safe.directory /github/workspace
 git tag
 git describe --tags
-if [ -z $(git describe --tags | grep -Po '^(v\d.\d.)(\d)-\d-.*' | cut -d. -f3 | cut -d- -f1) ];
+if [ -z $(git describe --tags | grep -Po '^(v\d*.\d*.)(\d*)-\d*-.*' | cut -d. -f3 | cut -d- -f1) ];
 	then
 		echo "::set-output name=tag::$(git describe --tags)"
 		exit 0;
 fi
-echo "::set-output name=tag::$(git describe --tags | grep -Po '^(v\d.\d.)(\d)-\d-.*' | cut -d. -f1,2).$(echo "$(git describe --tags | grep -Po '^(v\d.\d.)(\d)-\d-.*' | cut -d. -f3 | cut -d- -f1) + 1 " | bc)"
-git tag "$(git describe --tags | grep -Po '^(v\d.\d.)(\d)-\d-.*' | cut -d. -f1,2).$(echo "$(git describe --tags | grep -Po '^(v\d.\d.)(\d)-\d-.*' | cut -d. -f3 | cut -d- -f1) + 1 " | bc)"
+echo "::set-output name=tag::$(git describe --tags | grep -Po '^(v\d*.\d*.)(\d*)-\d*-.*' | cut -d. -f1,2).$(echo "$(git describe --tags | grep -Po '^(v\d*.\d*.)(\d*)-\d*-.*' | cut -d. -f3 | cut -d- -f1) + 1 " | bc)"
+git tag "$(git describe --tags | grep -Po '^(v\d*.\d*.)(\d*)-\d*-.*' | cut -d. -f1,2).$(echo "$(git describe --tags | grep -Po '^(v\d*.\d*.)(\d*)-\d*-.*' | cut -d. -f3 | cut -d- -f1) + 1 " | bc)"
 git push --tags
